2758435345a58c44a5741e84713e454dc33180f8
Cristian Avendaño Salgado <cristian.avendano@gruporeditos.com>
2025-12-19 11:16:16 -0500

    feat: Se la hora de encendido para pruebas

--- Activacion_AnalsysServices/Main.py
+++ Activacion_AnalsysServices/Main.py
@@ -18,7 +18,7 @@ print(f"Hora actual: {hora}:{minute}")
 
 
 # Es la hora de reanudar el servicio (6:00 AM)
-if hora == 6 and 0 <= minute <= 5:
+if hora == 11 and 0 <= minute <= 30:
     try:
         # Login en Azure antes de intentar reanudar
         login_result = az_login_service_principal()
@@ -58,7 +58,7 @@ if hora == 6 and 0 <= minute <= 5:
 
 
 # Es la hora de pausar el servicio (6:00 PM)
-if hora == 11 and 0 <= minute <= 30:
+if hora == 18 and 0 <= minute <= 5:
     try:
         # Login en Azure antes de intentar pausar
         login_result = az_login_service_principal()
@@ -82,6 +82,10 @@ if hora == 11 and 0 <= minute <= 30:
                 Estructura_Exitoso(estado.strip('"'))
                 break
             elif estado.strip('"') == "Suspending":
+                print("Esperando a que el estado sea 'Paused'...")
+                time.sleep(30)
+                reintento += 1
+            elif estado.strip('"') == "Pausing":
                 print("Esperando a que el estado sea 'Paused'...")
                 time.sleep(30)
                 reintento += 1
